980a8835c25af904961292ebbc8c686bf27993f8
Kandula Ashish Paul <ashish.paul@bounteous.com>
2026-02-17 18:49:03 +0530

    Improved things

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter ,Routes,Route} from "react-router-dom"
+import { Routes,Route} from "react-router-dom"
 import { PokemonPage } from "./Features/Pokemon/PokemonPage"
 import { PokemonDetailsPage } from "./Features/Pokemon/PokemonDetailsPage"
 const App = () => {
